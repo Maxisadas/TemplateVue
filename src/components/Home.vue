db60899603b4f0dfddc35d695a6ccb6231a5c33c
maxisadas <maxisadas@gmail.com>
2021-06-08 20:09:48 -0300

    template funcionando

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>HELLO WORLD</h1>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    props: [],
+    components: {},
+    mounted: {},
+    methods:{},
+    data() {
+        return {};
+    }
+}
+</script>
